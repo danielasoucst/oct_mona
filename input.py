2b718e81bf87f8c86a88cd9c4dea4364c6bcb4dc
daniela <daniela.soucst@gmail.com>
2017-06-16 23:19:50 -0300

    já achatando XD

--- input.py
+++ input.py
@@ -4,6 +4,7 @@ import cv2
 import heapq
 import numpy as np
 from matplotlib import pyplot as plt
+from scipy.ndimage.interpolation import shift
 
 def load_image(dir):
     lstImages = []
@@ -27,8 +28,8 @@ def apply_filter(image,metodo):
     return None
 
 def find_polynomial(lst_x,lst_y,largura):
-    print('x=: ',lst_x)
-    print('y=: ', lst_y)
+    # print('x=: ',lst_x)
+    # print('y=: ', lst_y)
     print('x,y',len(lst_x),len(lst_y))
 
     z = np.polyfit(lst_x, lst_y, 2)
@@ -58,33 +59,47 @@ def flat_image(image):
 
     if (len(pilot_rpe) > 0):
         ele = max(pilot_rpe)
-        limit = [ele for x in xrange(0, image.shape[1])]
+        limit = [ele for x in xrange(0, len(pilot_rpe))]
 
-    poly = find_polynomial(list_x, pilot_rpe,image.shape[1])
-    print pilot_rpe
-    print(limit)
+    poly = find_polynomial(list_x, pilot_rpe,len(list_x))
+    #print pilot_rpe
+    #print(limit)
+    diff = calculate_diff(poly, limit)
+    shi(image, list_x, diff)
     return pilot_rpe,limit,poly
-    # column = image[0:image.shape[0], 250]
-    # print column
 
 
+def calculate_diff(lstSup,lstInf):
+    result = [0 for x in range(0,len(lstSup))]
+    print('diff',len(lstSup),len(lstInf))
+    for i in range(0,len(lstSup)):
+        result[i] = round(lstInf[i]-lstSup[i],0)
+    # print('Inf: ',lstInf)
+    # print('SUp: ', lstSup)
+    print ('ajuste: ',result)
+    return result
+def shi(image,lst_x,diff):
+    index = 0
+    for x in lst_x:
+        # print('qts',diff[index])
+        column = image[0:image.shape[0], x]
+        image[0:image.shape[0], x] = shift(column, diff[index], cval=np.NaN)
+        index +=1
+        # print ('new-column',column)
 
+lstImagens = load_image('./Publication_Dataset/AMD3/TIFFs/8bitTIFFs/')
 
-lstImagens = load_image('./Publication_Dataset/AMD2/TIFFs/8bitTIFFs/')
-
-frame_denoise = apply_filter(lstImagens[1],'gauss')
+frame_denoise = apply_filter(lstImagens[4],'gauss')
 line,line2,poly = flat_image(frame_denoise)
-from skimage import feature
 
-edges2 = feature.canny(frame_denoise, sigma=2)
-print len(line)
+
 plt.figure()
 plt.subplot(121)
-plt.imshow(lstImagens[1], 'gray')
+plt.imshow(lstImagens[4], 'gray')
 plt.subplot(122)
 plt.imshow(frame_denoise, 'gray')
 plt.plot(line, '.', lw=1)
 # plt.plot(line2, 'ro', lw=1)
-plt.plot(poly, 'ro', lw=1)
+#plt.plot(poly, 'ro', lw=1)
 plt.show()
 
