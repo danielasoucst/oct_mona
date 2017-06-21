aaf8f92d0ae5e4526caeba2eb8e2a0b0889edb60
daniela <daniela.soucst@gmail.com>
2017-06-21 18:34:47 -0300

    com PCA

--- main.py
+++ main.py
@@ -2,6 +2,15 @@
 from matplotlib import pyplot as plt
 import input,flatten,cropping,features_extraction,arffGenerator
 import pickle
+from sklearn.decomposition import PCA
+
+def doPCA(features,num_comp):
+    pca = PCA(n_components=num_comp)
+    pca.fit(features)
+    features_redu = pca.transform(features)
+    print len(features_redu)
+    return features_redu
+
 
 def gerarHOGFeatures(sujeito):
     lstImagens = input.load_image('./Publication_Dataset/'+sujeito+'/TIFFs/8bitTIFFs/')
@@ -133,7 +142,25 @@ def getClass(pasta):
     if(inicio=='NOR'):
         return 'NORMAL'
     return None
-def geraBaseFeatures():
+
+def geraBaseAllFeatures():
+    volumes = []
+    volumes += ['AMD1','AMD2','AMD3','AMD4','AMD5','AMD6','AMD7','AMD8','AMD9','AMD10','AMD11','AMD12','AMD13','AMD14','AMD15']
+    volumes += ['DME1','DME2','DME3','DME4','DME5','DME6','DME7','DME8','DME9','DME10','DME11','DME12','DME13','DME14','DME15']
+    volumes += ['NORMAL1','NORMAL2','NORMAL3','NORMAL4','NORMAL5','NORMAL6','NORMAL7','NORMAL8','NORMAL9','NORMAL10','NORMAL11','NORMAL12','NORMAL13','NORMAL14','NORMAL15']
+
+    for vol in volumes:
+        print('Extraindo gabor features for: ',vol)
+        geraGABORFeatures(vol)
+    for vol in volumes:
+        print('Extraindo glcm features for: ',vol)
+        geraGLCMFeatures(vol)
+    for vol in volumes:
+        print('Extraindo lbp features for: ',vol)
+        geraLBPFeatures(vol)
+    print ("Fim...")
+
+def geraBaseFeaturesTest():
     volumes = []
     volumes += ['AMD1','AMD2','AMD3','AMD4','AMD5','AMD7','AMD8','AMD9','AMD10','AMD12','AMD13','AMD14']
     volumes += ['DME1','DME2','DME3','DME4','DME6','DME7','DME8','DME9','DME12','DME13','DME14','DME15']
@@ -205,15 +232,18 @@ def loadFeatures():
         features3 = pickle.load(fileObject)
 
         for i in range(0, len(features1)):
-            features4 = features1[i]
+            features4 = []
+            features4 += features1[i]
             features4 += features2[i]
             features4 += features3[i]
             GGLfeatures.append(features4)
 
+
         vectLabelsGGL += [getClass(vol) for i in range(0, len(features1))]
 
+    GGLfeatures = doPCA(GGLfeatures, 100)
     print ('Gerando arff file for gabor+ glcm + lbp')
-    arffGenerator.createArffFile('./gabor_glcm_lbp_features/GABORGLCMLBPDATASET', GGLfeatures, vectLabelsGGL,
+    arffGenerator.createArffFile('./gabor_glcm_lbp_features/GABORGLCMLBPPCADATASET', GGLfeatures, vectLabelsGGL,
                                  'AMD,DME,NORMAL', len(GGLfeatures[0]))
 
     # vetLabelsGaborGLCM = []
@@ -320,7 +350,7 @@ def loadFeatures():
 
 # gerarTrain(['./lbp_features/AMD1','./lbp_features/NORMAL11','./lbp_features/DME4'],['AMD','NORMAL','DME'],'AMDNORMALDME','AMD,NORMAL,DME','./lbp_features/')
 #geraBaseFeatures()
-loadFeaturesTest()
+loadFeatures()
 
 # features_extraction.apply_hog(crop)
 # features_extraction.hog2(crop)
